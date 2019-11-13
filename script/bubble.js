fea96c152eb2e51add3cba63fd2e44e039aba33b
HWooKim97 <rgd5454@naver.com>
2019-11-13 15:45:56 +0900

    fix bubble - add let

--- script/bubble.js
+++ script/bubble.js
@@ -2,7 +2,7 @@
 
 function bubbleSort(arr){
     for(let i = 0; i < arr.length; i++){
-        for(j = 1; j < arr.length - i; j++){
+        for(let j = 1; j < arr.length - i; j++){
             if(arr[j - 1] > arr[j])
                 [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
         }
